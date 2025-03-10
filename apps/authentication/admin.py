578a286b018fcce61de2b99087caa036e1a64e09
rafiulislam18 <cmmnacrafiulislam0170@gmail.com>
2025-03-11 03:13:11 +0600

    feat(apps/authentication): register models to admin

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import CustomUser, Device
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'highest_score', 'total_played')
+    search_fields = ('username',)
+    ordering = ('-highest_score',)
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'device_id', 'last_login')
+    search_fields = ('user__username', 'device_id')
+    ordering = ('-last_login',)
