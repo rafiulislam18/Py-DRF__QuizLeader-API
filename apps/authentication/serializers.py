d8d9499c52c490562f8a287825d23cd05d51744c
rafiulislam18 <cmmnacrafiulislam0170@gmail.com>
2025-03-11 03:18:53 +0600

    feat(apps/authentication): define serializers

---
+++ apps/authentication/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import CustomUser, Device
+from django.core.exceptions import ValidationError
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'highest_score', 'total_played']
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+    device_id = serializers.CharField()
+    
+    def validate(self, data):
+        if len(data['password']) < 8:
+            raise ValidationError("Password must be at least 8 characters")
+        return data
+
+
+class DeviceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Device
+        fields = ['device_id', 'last_login']
