64d6d3fd7765a1e1d048b4898088f0e1f86247bb
rafiulislam18 <cmmnacrafiulislam0170@gmail.com>
2025-03-21 01:10:27 +0600

    feat(apps/quiz): implement SubjectLeaderboardPagination

--- apps/quiz/paginators.py
+++ apps/quiz/paginators.py
@@ -17,3 +17,10 @@ class QuestionListPagination(PageNumberPagination):
     page_size = 15
     page_size_query_param = 'page_size'
     max_page_size = 30
+
+
+class SubjectLeaderboardPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+
