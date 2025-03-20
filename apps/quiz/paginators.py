6fe37cf62b8087a04183f77f6f4ab2acf6c78a5d
rafiulislam18 <cmmnacrafiulislam0170@gmail.com>
2025-03-20 23:58:52 +0600

    feat(apps/quiz): implement LessonListPagination

--- apps/quiz/paginators.py
+++ apps/quiz/paginators.py
@@ -5,3 +5,9 @@ class SubjectListPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 25
+
+
+class LessonListPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 50
