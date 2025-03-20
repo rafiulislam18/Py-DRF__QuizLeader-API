f51d28e733e03791a14c066efdff35f133700563
rafiulislam18 <cmmnacrafiulislam0170@gmail.com>
2025-03-21 01:13:41 +0600

    config(apps/quiz): config url for GlobalLeaderboardView

--- apps/quiz/urls.py
+++ apps/quiz/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import QuizStartView, QuizSubmitView, SubjectLeaderboardView, SubjectView, LessonView, QuestionView
+from .views import QuizStartView, QuizSubmitView, SubjectLeaderboardView, GlobalLeaderboardView, SubjectView, LessonView, QuestionView
 
 
 urlpatterns = [
     path('game/start/<int:lesson_id>/', QuizStartView.as_view(), name='quiz_start'),
     path('game/submit/<int:attempt_id>/', QuizSubmitView.as_view(), name='quiz_submit'),
     path('leaderboard/<int:subject_id>/', SubjectLeaderboardView.as_view(), name='subject_leaderboard'),
+    path('global-leaderboard/', GlobalLeaderboardView.as_view(), name='global_leaderboard'),
     path('subjects/', SubjectView.as_view(), name='subject'),
     path('lessons/<int:subject_id>/', LessonView.as_view(), name='lesson'),
     path('questions/<int:lesson_id>/', QuestionView.as_view(), name='question'),
