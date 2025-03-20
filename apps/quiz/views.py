aff0be6d347bad60a61af8acd3302d1cc5a19a8a
rafiulislam18 <cmmnacrafiulislam0170@gmail.com>
2025-03-21 01:07:54 +0600

    feat(apps/quiz): implement QuizSubmitView

--- apps/quiz/views.py
+++ apps/quiz/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.db.models import Max, Avg, Count, F
 from .models import Subject, Lesson, Question, QuizAttempt
-from .serializers import SubjectSerializer, LessonSerializer, LessonResponseSerializer, QuestionSerializer, QuestionResponseSerializer, QuizStartResponseSerializer, QuestionPaginatedResponseSerializer, SubjectPaginatedResponseSerializer, LessonPaginatedResponseSerializer
+from .serializers import SubjectSerializer, LessonSerializer, LessonResponseSerializer, QuestionSerializer, QuestionResponseSerializer, QuizStartResponseSerializer, QuizSubmitSerializer, QuizSubmitResponseSerializer, QuestionPaginatedResponseSerializer, SubjectPaginatedResponseSerializer, LessonPaginatedResponseSerializer
 from django.db import transaction
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -86,6 +86,109 @@ class QuizStartView(APIView):
         
         except Exception as e:
             logger.error(f"Error in QuizStartView.post(): {str(e)}", exc_info=True)  # Log the error for debugging
+            return Response(
+                {"detail": "An error occurred while processing your request."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+class QuizSubmitView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    # Submit quiz answers, calculate score & update user profile
+    @swagger_auto_schema(
+        tags=["Quiz-Game"],
+        operation_description="Submit quiz answers & get score (get attempt_id by starting a quiz)",
+        manual_parameters=[
+            openapi.Parameter(
+                'attempt_id',
+                openapi.IN_PATH,
+                description="ID of the quiz attempt to submit answers for",
+                type=openapi.TYPE_INTEGER,
+                required=True
+            )
+        ],
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'answers': openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    additional_properties=openapi.Schema(type=openapi.TYPE_STRING),
+                    description='Dictionary of "question_id": "selected_option"'
+                )
+            },
+            required=['answers'],
+            example={
+                "answers": {
+                    "101": "1",
+                    "102": "3",
+                    "103": "2"
+                }
+            }
+        ),
+        responses={
+            200: openapi.Response('Success: Quiz answers submition successful', QuizSubmitResponseSerializer),
+            400: 'Error: Bad request',
+            401: 'Error: Unauthorized',
+            404: 'Error: Not found',
+            500: 'Error: Internal server error'
+        }
+    )
+    @transaction.atomic
+    def post(self, request, attempt_id):
+        try:
+            attempt = QuizAttempt.objects.select_for_update().get(
+                id=attempt_id,
+                user=request.user
+            )
+            # Return detailed message if quiz is already completed
+            if attempt.completed == True:
+                return Response(
+                    {"detail": "Quiz already completed."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            
+            # Proceed to data validation
+            serializer = QuizSubmitSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            
+            answers = serializer.validated_data['answers']  # {"question_id": "selected_option"}
+            questions = Question.objects.filter(
+                id__in=answers.keys()
+            ).values('id', 'correct_answer')
+            
+            score = 0
+            for question in questions:
+                user_answer = int(answers[str(question['id'])])
+                if user_answer == question['correct_answer']:
+                    score += 1
+            
+            attempt.score = score
+            attempt.completed = True
+            attempt.save()
+            
+            # Update user profile
+            user = request.user
+            user.total_played += 1
+            user.highest_score = max(user.highest_score, score)
+            user.save()
+            
+            return Response(QuizSubmitResponseSerializer(attempt).data)
+        
+        except QuizAttempt.DoesNotExist:
+            return Response(
+                {"detail": "Quiz attempt not found."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        except ValidationError as e:
+            return Response(
+                {"detail": str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        except Exception as e:
+            logger.error(f"Error in QuizSubmitView.post(): {str(e)}", exc_info=True)  # Log the error for debugging
             return Response(
                 {"detail": "An error occurred while processing your request."},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
