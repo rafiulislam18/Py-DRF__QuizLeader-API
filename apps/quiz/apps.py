609b0137a769397531ec710564c8ad251a6905c1
rafiulislam18 <cmmnacrafiulislam0170@gmail.com>
2025-03-20 23:48:39 +0600

    feat(apps/quiz): implement signal to enforce 30 question limit per lesson

--- apps/quiz/apps.py
+++ apps/quiz/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class QuizConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.quiz'
+
+    def ready(self):
+        # Import and connect the signals
+        import apps.quiz.signals
---
+++ apps/quiz/signals.py
@@ -0,0 +1,10 @@
+from django.core.exceptions import ValidationError
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import Question
+
+
+@receiver(pre_save, sender=Question)
+def enforce_question_limit(sender, instance, **kwargs):
+    if instance.lesson.questions.count() >= 30 and not instance.pk:
+        raise ValidationError("A lesson cannot have more than 30 questions.")
