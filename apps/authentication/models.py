f91df6973ae5cabcaf1f3d2e596f744914441d06
rafiulislam18 <cmmnacrafiulislam0170@gmail.com>
2025-03-20 16:17:37 +0600

    revert(apps/authentication): feat: define Device model

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -13,15 +13,3 @@ class CustomUser(AbstractUser):
             Index(fields=['username'], name='username_idx'),
             Index(fields=['highest_score'], name='high_score_idx'),
         ]
-
-
-# Store device-specific authentication data (e.g. front-end generated "device ID")
-class Device(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    device_id = models.CharField(max_length=100, unique=True)
-    last_login = models.DateTimeField(auto_now=True)
-    
-    class Meta:
-        indexes = [
-            Index(fields=['device_id'], name='device_id_idx'),
-        ]
