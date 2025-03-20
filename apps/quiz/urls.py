e62ca7eec9b5b78f3cf24e533cba5691a877f6cc
rafiulislam18 <cmmnacrafiulislam0170@gmail.com>
2025-03-21 01:04:34 +0600

    config(apps/quiz): config url for QuizStartView

--- apps/quiz/urls.py
+++ apps/quiz/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SubjectView, LessonView, QuestionView
+from .views import QuizStartView, SubjectView, LessonView, QuestionView
 
 
 urlpatterns = [
+    path('game/start/<int:lesson_id>/', QuizStartView.as_view(), name='quiz_start'),
     path('subjects/', SubjectView.as_view(), name='subject'),
     path('lessons/<int:subject_id>/', LessonView.as_view(), name='lesson'),
     path('questions/<int:lesson_id>/', QuestionView.as_view(), name='question'),
