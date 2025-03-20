734b731f8993619518eb717e7c9c29fd1705d4d6
rafiulislam18 <cmmnacrafiulislam0170@gmail.com>
2025-03-21 00:19:30 +0600

    config(apps/quiz): config url for QuestionView

--- apps/quiz/urls.py
+++ apps/quiz/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SubjectView, LessonView
+from .views import SubjectView, LessonView, QuestionView
 
 
 urlpatterns = [
     path('subjects/', SubjectView.as_view(), name='subject'),
     path('lessons/<int:subject_id>/', LessonView.as_view(), name='lesson'),
+    path('questions/<int:lesson_id>/', QuestionView.as_view(), name='question'),
 ]
