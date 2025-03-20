7551685ebbbad1725ce50556393d4fb732647950
rafiulislam18 <cmmnacrafiulislam0170@gmail.com>
2025-03-20 23:58:25 +0600

    feat(apps/quiz): implement LessonSerializer, LessonResponseSerializer & LessonPaginatedResponseSerializer

--- apps/quiz/serializers.py
+++ apps/quiz/serializers.py
@@ -25,3 +25,40 @@ class SubjectPaginatedResponseSerializer(serializers.Serializer):
         help_text="List of subjects",
         many=True
     )
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(
+        help_text="Title of the lesson",
+        max_length=200
+    )
+
+    class Meta:
+        model = Lesson
+        fields = ['title']
+
+
+class LessonResponseSerializer(serializers.ModelSerializer):
+    subject = SubjectSerializer(read_only=True)
+    
+    class Meta:
+        model = Lesson
+        fields = ['id', 'title', 'subject']
+
+
+class LessonPaginatedResponseSerializer(serializers.Serializer):
+    count = serializers.IntegerField(
+        help_text="Total number of lessons"
+    )
+    next = serializers.CharField(
+        help_text="URL for next page(null if no more pages)",
+        allow_null=True
+    )
+    previous = serializers.CharField(
+        help_text="URL for previous page (null if first page)",
+        allow_null=True
+    )
+    results = LessonResponseSerializer(
+        help_text="List of lessons",
+        many=True
+    )
