d8de24c5eff784735f06bc353d6559f6f31dbd37
rafiulislam18 <cmmnacrafiulislam0170@gmail.com>
2025-03-21 01:11:31 +0600

    feat(apps/quiz): implement SubjectLeaderboardView

--- apps/quiz/views.py
+++ apps/quiz/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.db.models import Max, Avg, Count, F
 from .models import Subject, Lesson, Question, QuizAttempt
-from .serializers import SubjectSerializer, LessonSerializer, LessonResponseSerializer, QuestionSerializer, QuestionResponseSerializer, QuizStartResponseSerializer, QuizSubmitSerializer, QuizSubmitResponseSerializer, QuestionPaginatedResponseSerializer, SubjectPaginatedResponseSerializer, LessonPaginatedResponseSerializer
+from .serializers import SubjectSerializer, LessonSerializer, LessonResponseSerializer, QuestionSerializer, QuestionResponseSerializer, QuizStartResponseSerializer, QuizSubmitSerializer, QuizSubmitResponseSerializer, LeaderboardResponseSerializer, LeaderboardPaginatedResponseSerializer, QuestionPaginatedResponseSerializer, SubjectPaginatedResponseSerializer, LessonPaginatedResponseSerializer
 from django.db import transaction
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -11,7 +11,7 @@ from django.core.exceptions import ValidationError as DjangoValidationError
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 import random
-from .paginators import SubjectListPagination, LessonListPagination, QuestionListPagination
+from .paginators import SubjectListPagination, LessonListPagination, QuestionListPagination, SubjectLeaderboardPagination
 from django.core.cache import cache
 import logging
 
@@ -189,6 +189,94 @@ class QuizSubmitView(APIView):
         
         except Exception as e:
             logger.error(f"Error in QuizSubmitView.post(): {str(e)}", exc_info=True)  # Log the error for debugging
+            return Response(
+                {"detail": "An error occurred while processing your request."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+class SubjectLeaderboardView(APIView):
+    permission_classes = [AllowAny]
+    pagination_class = SubjectLeaderboardPagination
+
+    # Get subject-specific leaderboard (top 10)
+    @swagger_auto_schema(
+        tags=["Quiz-Leaderboard"],
+        operation_description="Get subject-specific leaderboard (top 10 players)",
+        manual_parameters=[
+            openapi.Parameter(
+                'subject_id',
+                openapi.IN_PATH,
+                description="ID of the subject",
+                type=openapi.TYPE_INTEGER,
+                required=True
+            ),
+            openapi.Parameter(
+                'page',
+                openapi.IN_QUERY,
+                description="Page number (default: 1)",
+                type=openapi.TYPE_INTEGER,
+                required=False
+            ),
+            openapi.Parameter(
+                'page_size',
+                openapi.IN_QUERY,
+                description=f"Number of results per page (default: {SubjectLeaderboardPagination.page_size}, max: {SubjectLeaderboardPagination.max_page_size})",
+                type=openapi.TYPE_INTEGER,
+                required=False
+            )
+        ],
+        responses={
+            200: openapi.Response('Success: Get subject-specific leaderboard successful', LeaderboardPaginatedResponseSerializer),
+            400: 'Error: Bad request',
+            404: 'Error: Not found',
+            500: 'Error: Internal server error'
+        }
+    )
+    def get(self, request, subject_id):
+        try:
+            attempts = QuizAttempt.objects.filter(
+                lesson__subject__id=subject_id,  # Filter by subject through lesson
+                completed=True
+            ).values(username=F('user__username')).annotate(
+                high_score=Max('score'),
+                avg_score=Avg('score'),
+                total_attempts=Count('id')
+            ).order_by('-high_score')[:10]
+
+            if not attempts.exists():
+                return Response(
+                        {"detail": "No data found for the given subject."},
+                        status=status.HTTP_404_NOT_FOUND
+                    )
+
+            # Enforce pagination
+            paginator = self.pagination_class()
+            page = paginator.paginate_queryset(attempts, request)
+            if page is not None:
+                serializer = LeaderboardResponseSerializer(page, many=True)
+                return paginator.get_paginated_response(serializer.data)
+            
+            # If pagination is not applied, throw an error
+            return Response(
+                {"error": "Pagination is required for this endpoint."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        except QuizAttempt.DoesNotExist:
+            return Response(
+                {"detail": "Subject not found."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        except NotFound as e:
+            return Response(
+                {"detail": str(e)},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        except Exception as e:
+            logger.error(f"Error in SubjectLeaderboardView.get(): {str(e)}", exc_info=True)  # Log the error for debugging
             return Response(
                 {"detail": "An error occurred while processing your request."},
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR
