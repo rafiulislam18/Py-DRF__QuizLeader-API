9460d0faa556052d409fac13469b2f9ca972c673
rafiulislam18 <cmmnacrafiulislam0170@gmail.com>
2025-03-21 17:45:27 +0600

    style(apps/authentication): reformat code to pretify

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -6,7 +6,11 @@ from .models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'username', 'highest_score', 'total_played', 'is_staff'
+        'id',
+        'username',
+        'highest_score',
+        'total_played',
+        'is_staff'
     )
     search_fields = ('username',)
     ordering = ('-highest_score',)
